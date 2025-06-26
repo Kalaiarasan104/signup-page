80310f1a38eaef28cf3fe0a0b82d6f3a5a38ef4f
kalaiarasan.anandan@anywhere.co <kalaiarasan.anandan@anywhere.co>
2025-06-26 14:44:10 +0530

    Add initial project files including HTML structure, CSS styles, JavaScript functionality, and design assets.

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.signup-form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // You can add logic here to validate inputs
+  alert('Form submitted!');
+});
